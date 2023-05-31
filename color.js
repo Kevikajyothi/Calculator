450270bbd54b6ef6fb4fc1a113324b0c0b58de2a
Kevikajyothi <jyothikevika2@gmail.com>
2023-05-31 12:39:49 +0530

    test

---
+++ color.js
@@ -0,0 +1,19 @@
+function cc_MouseOver(){
+    document.getElementById('mover').style.backgroundColor="yellow"
+}
+
+function cc_MouseOut(){
+    document.getElementById('mout').style.backgroundColor="pink"
+}
+
+let cc_Click=()=>{
+    document.getElementById('click').style.backgroundColor="red"
+}
+
+function cc_Focus(){
+    document.getElementById('focusid').style.backgroundColor="blue"
+}
+
+function cc_Blur(){
+   document.getElementById('blurid').style.backgroundColor="red"
+}
